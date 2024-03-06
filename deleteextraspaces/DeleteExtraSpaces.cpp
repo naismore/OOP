0b9221a56dd934954d95e6a14ee11273bc2aa8ab
naismore <anxismore@gmail.com>
2024-03-06 19:32:41 +0300

    lw2.1

---
+++ deleteextraspaces/DeleteExtraSpaces.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string RemoveExtraSpaces(std::string const& arg)
+{
+    bool space_sleep = true;
+    string resultString;
+    for (char i : arg)
+    {
+        if (i == ' ' && space_sleep)
+        {
+            continue;
+        }
+        if (i == ' ')
+        {
+            resultString.push_back(i);
+            space_sleep = true;
+        }
+        else
+        {
+            resultString.push_back(i);
+            space_sleep = false;
+        }
+    }
+    return resultString;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ tests/tests.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include <catch.hpp>
+#define CATCH_CONFIG_MAIN
+
+TEST_CASE("")
+{
+
+}
