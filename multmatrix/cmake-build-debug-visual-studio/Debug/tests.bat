8e0f83ee1626a2f66aa3e0a3e2e06b00a1afd250
naismore <anxismore@gmail.com>
2024-03-06 17:06:56 +0300

    lw1

--- multmatrix/cmake-build-debug-visual-studio/Debug/tests.bat
+++ multmatrix/cmake-build-debug-visual-studio/Debug/tests.bat
@@ -3,17 +3,26 @@ set PROGRAM=multmatrix.exe
 
 %PROGRAM% matrix1.txt empty.txt
 if NOT ERRORLEVEL 1 goto err
+fc.exe open_empty_file.txt out.txt
+if ERRORLEVEL 1 goto err
 
 %PROGRAM% matrix1.txt not_created_file.txt
 if NOT ERRORLEVEL 1 goto err
+fc.exe open_not_created_file.txt out.txt
+if ERRORLEVEL 1 goto err
 
 %PROGRAM% matrix1.txt bad_matrix2.txt
 if NOT ERRORLEVEL 1 goto err
+fc.exe open_bad_matrix.txt out.txt
+if ERRORLEVEL 1 goto err
 
 %PROGRAM% matrix1.txt matrix2.txt
 if ERRORLEVEL 1 goto err
+fc.exe open_successful_matrix.txt out.txt
+if ERRORLEVEL 1 goto err
 
 echo Program testing successful
+
 
 :err
 echo Program testing error
--- multmatrix/main.cpp
+++ multmatrix/main.cpp
@@ -1,139 +1,114 @@
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//#include <sstream>
-//#include <array>
-//
-//// Разбить на функции, чтение матрицы, вывод, перемножение.
-//// std array для хранения массива
-//
-//using namespace std;
-//
-//array<array<int, 3>, 3> OpenMatrixFile(string path)
-//{
-//    ifstream in(path);
-//    if (!in.is_open())
-//    {
-//
-//    }
-//}
-//
-//int main(int argc, char* args[])
-//{
-//    if (argc != 3)
-//    {
-//        cout << "Usage " << args[0] << "<matrix file1> <matrix file2>\n";
-//        return 1;
-//    }
-//
-//    ifstream inMatrix1(args[1]);
-//    ifstream inMatrix2(args[2]);
-//
-//    if (!inMatrix1.is_open() || !inMatrix2.is_open())
-//    {
-//        cout << "Error to open files\n";
-//        return 1;
-//    }
-//
-//    array<array<int, 3>, 3> a{ {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}} };
-//
-//
-//    double matrix1[3][3];
-//    double matrix2[3][3];
-//    double matrix3[3][3];
-//
-//    string line;
-//    for (int i = 0; i < 3; ++i) {
-//        if (getline(inMatrix1, line)) {
-//            istringstream iss(line);
-//            for (int j = 0; j < 3; ++j) {
-//                string value;
-//                if (!(iss >> value) || value.find_first_not_of("0123456789.-") != string::npos) {
-//                    cerr << "Error reading matrix element\n";
-//                    return 1;
-//                }
-//                matrix1[i][j] = stof(value);
-//            }
-//        }
-//        else
-//        {
-//            cout << "Error reading line: Empty line\n";
-//            return 1;
-//        }
-//    }
-//
-//    line.clear();
-//    for (int i = 0; i < 3; ++i) {
-//        if (getline(inMatrix2, line)) {
-//            istringstream iss(line);
-//            for (int j = 0; j < 3; ++j) {
-//                string value;
-//                if (!(iss >> value) || value.find_first_not_of("0123456789.-") != string::npos) {
-//                    cerr << "Error reading matrix element at position\n";
-//                    return 1;
-//                }
-//                matrix2[i][j] = stof(value);
-//            }
-//        }
-//        else
-//        {
-//            cout << "Error reading line: Empty line\n";
-//            return 1;
-//        }
-//    }
-//
-//    for (int i = 0; i < 3; ++i) {
-//        for (int j = 0; j < 3; ++j) {
-//            matrix3[i][j] = 0;
-//            for (int k = 0; k < 3; ++k) {
-//                matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
-//            }
-//        }
-//    }
-//
-//    for (auto & i : matrix3) {
-//        for (double j : i) {
-//            cout << j << " ";
-//        }
-//        cout << endl;
-//    }
-//
-//    return 0;
-//}
-
 #include <iostream>
 #include <array>
 #include <fstream>
+#include <sstream>
+
+// Батник дописать
 
 using namespace std;
 
-const int MATRIX_HEIGHT = 2;
-const int MATRIX_WIDTH = 5;
+const int MATRIX_HEIGHT = 3;
+const int MATRIX_WIDTH = 3;
 
-array<int, MATRIX_WIDTH * MATRIX_HEIGHT> OpenFile(string path)
+typedef array<array<float, MATRIX_HEIGHT>, MATRIX_WIDTH> MatrixType;
+
+void PrintErrorToFile(const string& message, const string& path)
 {
-    ifstream in(path);
+    ofstream out(path);
+    out << message << endl;
+}
 
-    if (!in.is_open())
+void PrintMatrixToFile(MatrixType matrix, const string& path)
+{
+    ofstream out(path);
+    for (auto & i : matrix)
     {
-        throw string{"Error to open file"};
-    }
-    else
-    {
-
+        for (double j : i)
+        {
+            out << j << " ";
+        }
+        out << endl;
     }
 }
 
-int main()
+void PrintMatrix(MatrixType matrix)
 {
-
-
-    ifstream in("m1.txt");
-
-    array<int, MATRIX_HEIGHT * MATRIX_WIDTH> arr {};
-    for (int i : arr)
+    for (auto & i : matrix)
     {
-        cout << i << endl;
+        for (double j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
     }
-    return 1;
+}
+
+bool OpenFile(const string& path, MatrixType& matrix)
+{
+    ifstream in(path);
+
+    string line;
+    for (int i = 0; i < 3; ++i)
+    {
+        if (getline(in, line))
+        {
+            istringstream iss(line);
+            for (int j = 0; j < 3; ++j)
+            {
+                string value;
+                if (!(iss >> value) || value.find_first_not_of("0123456789.-") != string::npos)
+                {
+                    return false;
+                }
+
+                matrix[i][j] = stof(value);
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void MultiplicationMatrix(MatrixType& matrix1, MatrixType& matrix2, MatrixType& matrix3)
+{
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            matrix3[i][j] = 0;
+            for (int k = 0; k < 3; ++k)
+            {
+                matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+}
+
+int main(int argc, char* args[])
+{
+    if (argc != 3)
+    {
+        cout << "Usage " << args[0] << "<matrix file1> <matrix file2>\n";
+        return 1;
+    }
+
+    MatrixType matrix1{};
+    MatrixType matrix2{};
+    MatrixType matrix3{};
+
+    if (!OpenFile(args[1], matrix1) || !OpenFile(args[2], matrix2))
+    {
+        cout << "Error to open files\n";
+        PrintErrorToFile("Error to open files", "out.txt");
+        return 1;
+    }
+
+    MultiplicationMatrix(matrix1, matrix2, matrix3);
+
+    PrintMatrixToFile(matrix3, "out.txt");
+    PrintMatrix(matrix3);
+    return 0;
 }
