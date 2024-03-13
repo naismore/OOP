894cec2fe0d3ec8b08a792c9966387c5728a2886
naismore <anxismore@gmail.com>
2024-03-13 13:37:24 +0300

    lw3.2

---
+++ minidictionary/minidictionary.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+bool OpenFile(const string& path)
+{
+	ifstream in(path);
+
+	if (!in.is_open())
+	{
+		return false;
+	}
+	
+	string line;
+	while (getline(in, line))
+	{
+		if (!line.empty())
+		{
+			istringstream iss(line);
+			string word, translation;
+
+			iss >> word >> translation;
+			dictionary[word] = translation;
+		}
+	}
+	return true;
+}
+
+map<string, string> dictionary;
+
+int Main(int argc, char* args[])
+{
+
+}
