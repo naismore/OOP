7ad3543ca21ce4039c81f32d559c12c58e782644
naismore <anxismore@gmail.com>
2024-05-15 13:27:31 +0300

    lw4

--- lw4/CBody.cpp
+++ lw4/CBody.cpp
@@ -1,6 +1,21 @@
 #include "CBody.h"
 
-double CBody GetDensity()
+double CBody::GetDensity()
+{
+
+}
+
+double CBody::GetVolume()
+{
+
+}
+
+double CBody::GetMass()
+{
+
+}
+
+std::string ToString()
 {
 
 }
---
+++ lw4/CSphere.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "CBody.h"
+
+class CSphere : public CBody
+{
+
+};
+
